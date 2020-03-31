e9f30bed642594f67a5761ec0c5733301a72e0b8
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-03-31 01:13:01 +0000

    Automatically backed up by Learn

--- spec/010_music_library_controller_spec.rb
+++ spec/010_music_library_controller_spec.rb
@@ -24,7 +24,7 @@ describe 'MusicLibraryController' do
       expect(MusicImporter).to receive(:new).with('./spec/fixtures/mp3s').and_return(music_importer)
 
       MusicLibraryController.new('./spec/fixtures/mp3s')
-      expect(Song.all.size).to eq(4)
+      expect(Song.all.size).to eq(5)
       expect(Artist.all.size).to eq(3)
       expect(Genre.all.size).to eq(4)
     end
