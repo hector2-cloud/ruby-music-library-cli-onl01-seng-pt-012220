3e1e1693b3e902c5fe9bb8768ee27031438b87ed
awesome-writer-4373 <hectoraperez1999@gmail.com>
2020-03-31 01:14:38 +0000

    Automatically backed up by Learn

--- spec/009_music_importer_spec.rb
+++ spec/009_music_importer_spec.rb
@@ -77,7 +77,7 @@ describe "MusicImporter#import" do
     music_importer.import
 
     expect(Song.all.size).to eq(5)
-    expect(Artist.all.size).to eq(3)
+    expect(Artist.all.size).to eq(4)
     expect(Genre.all.size).to eq(4)
 
     expect(Song.find_by_name("Green Aisles").artist.name).to eq("Real Estate")
